6d7212400a683f5a70a12fce126ed990879093e8
alessandro <abiank@users.noreply.github.com>
2014-08-26 03:42:46 +0200

    Update myStartupScript.js

--- js/myStartupScript.js
+++ js/myStartupScript.js
@@ -1,5 +1,34 @@
 console.log("LOADed!");
 
+   acemenu = Ext.create('Ext.menu.Menu', {
+        id: 'aceMenu',
+        items: [{
+        text: 'ExtEditor Newtab',
+        url: 'static/support/exteditor/edit.htm',
+        target:_room+"SCRIPT",
+        //baseParams: {  q: 'urlparam'},
+        tooltip: 'Click here for help.'
+    },
+    {
+        text: 'ExtEditor',
+        tooltip: 'External editor in neue fenster yeah.',
+        handler:function(){window.open("static/support/exteditor/edit.htm",_room+"SCRIPT","location=0")}
+    },
+    {
+        text: 'ExtEditor inwindow',
+        tooltip: '',
+        handler:function(){openwin("static/support/exteditor/edit.htm")}
+    }        
+            
+            
+            
+            ]
+    });
+    
+    tb.add({text:"ACE",menu:acemenu});
+
+/*
+  
     tb.add({
         text: 'ExtEditor Newtab',
         url: 'static/support/exteditor/edit.htm',
@@ -14,4 +43,4 @@ console.log("LOADed!");
         tooltip: 'External editor in neue fenster yeah.',
         handler:function(){window.open("static/support/exteditor/edit.htm",_room+"SCRIPT","location=0")}
     });
-
+*/
