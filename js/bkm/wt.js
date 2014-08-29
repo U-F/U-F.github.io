a7acec0793c1ff56beee7cc1b674a9030d41d074
undefined undefined <yamacorp@gmail.com>
2014-08-29 20:15:11 +0000

    start bklet

--- js/bkm/wt.js
+++ js/bkm/wt.js
@@ -1,7 +1,12 @@
 if (localStorage.getItem("wt_integration_sheet_url")==null)  localStorage.setItem("wt_integration_sheet_url","http://s-ec.github.io");
 
 sheetupdate = function() {
-    magicsheetwin.postMessage({hello:121},"*");
+    its=document.querySelectorAll(".item");
+    var pkt=[];
+    for (ea in its) {pkt.push({nam:its[ea].querySelector(".name").innerText,ask:its[ea].querySelector(".ask").innerText,bid:its[ea].querySelector(".bid").innerText}); }
+
+
+    magicsheetwin.postMessage(pkt,"*"); // we could really be sending commands instead... will update once an API is ready
 }
 
 magicsheetwin=window.open(localStorage.getItem("wt_integration_sheet_url"),"_blank");
