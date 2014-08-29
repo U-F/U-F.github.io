e8bf2be9c141d2bb83ddf767ce1c952223879d84
undefined undefined <yamacorp@gmail.com>
2014-08-29 20:04:10 +0000

    start bklet

--- js/bkm/wt.js
+++ js/bkm/wt.js
@@ -1,10 +1,10 @@
+if (localStorage.getItem("wt_integration_sheet_url")==null)  localStorage.setItem("wt_integration_sheet_url","http://s-ec.github.io");
+
 sheetupdate = function() {
     magicsheetwin.postMessage({hello:121},"*");
-    
-    
 }
 
-magicsheetwin=window.open("http://s-ec.github.com");
+magicsheetwin=window.open(localStorage.getItem("wt_integration_sheet_url"),"_blank");
 globint = setInterval(sheetupdate,10000);
 
-console.log("bkm loaded");
+console.log("wt part2 loaded");
