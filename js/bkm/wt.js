66c52449d93906f8f499f0708cc59afa03df43f4
undefined undefined <yamacorp@gmail.com>
2014-09-13 21:55:05 +0000

    test

--- js/bkm/wt.js
+++ js/bkm/wt.js
@@ -2,16 +2,15 @@ if (localStorage.getItem("wt_integration_sheet_url")==null)  localStorage.setIte
 
 sheetupdate = function() {
     its=document.querySelectorAll(".item");
-    var pkt=[];
+    var pkt={};
     for (ea in its) {
         if (its[ea]){
         console.log(its[ea]);
         try{
-        pkt.push({nam:its[ea].querySelector(".name").innerText,ask:its[ea].querySelector(".ask").innerText,bid:its[ea].querySelector(".bid").innerText}); } catch(e) {}
+            
+        pkt[its[ea].querySelector(".name").innerText.replace(" ","")]={ask:its[ea].querySelector(".ask").innerText,bid:its[ea].querySelector(".bid").innerText}; } catch(e) {}
         }
     }
-
-
     magicsheetwin.postMessage(pkt,"*"); // we could really be sending commands instead... will update once an API is ready
 }
 
