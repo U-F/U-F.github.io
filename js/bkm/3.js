cf16c9e9676f39a8a1b4e339708e6c2bb2628ec5
ale <you@example.com>
2015-03-17 02:38:53 +0100

    hmmmmmm

--- js/bkm/3.js
+++ js/bkm/3.js
@@ -9,10 +9,10 @@ for (ea in u) {
     try {
     c++;
     TNAME=t[+ea + 1].innerText.replace(/[\u0400-\uF8FF]/g, '')
-    console.log(u[ea].value + " " + t[+ea + 1].innerText + " " + TNAME);
+    //console.log(u[ea].value + " " + t[+ea + 1].innerText + " " + TNAME);
     mtxt+="File"+c+"="+u[ea].value+"\nTitle"+c+"="+TNAME+"\n";
     if (TNAME.indexOf("onosciuto")>-1) {TNAME=TNAME.replace("sciuto","sciuto"+Math.random())}
-    wtxt+="curl '"+u[ea].value+"' > '"+sanitiz(TNAME)+".mp3'\n";
+    wtxt+="curl '"+u[ea].value+"' > '"+(TNAME)+".mp3'\n";
     } catch(e) {}
 }
 otxt+=c+"\n"+mtxt;
