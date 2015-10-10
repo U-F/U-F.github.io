cf2aa09bfe6302c6f4645c0fea7031701846cd98
ale <you@example.com>
2015-10-10 03:03:10 +0200

    cant seem to instantiate p5js on an external window for now

--- js/p5load.js
+++ js/p5load.js
@@ -1,4 +1,29 @@
-p5loaded=function(){alert("p5.js loaded");}
+p5loaded=function(){
+displayMsg("
+// Try this, wont work in external window for now
+stoca= opendivwin('contents','wintitle',612,612);
 
-loadScript("http://cdnjs.com/libraries/p5.js",p5loaded)
+var sketch = function( p ) {
+
+  var x = 100; 
+  var y = 100;
+
+  p.setup = function() {
+    p.createCanvas(600, 600);
+  };
+
+  p.draw = function() {
+    p.background(0);
+    p.fill(255*Math.random(),128,128,Math.random()*128);
+    p.rect(x,y,50,50);
+  };
+};
+
+myp5 = new p5(sketch,stoca);
+
+	")
+
+}
+
+loadScript("http://cdnjs.cloudflare.com/ajax/libs/p5.js/0.4.15/p5.js",p5loaded)
 
