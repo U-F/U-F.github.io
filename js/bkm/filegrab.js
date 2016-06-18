3495d5c1fd19146be6a56b1a0f5500bd162e1a35
ale <you@example.com>
2016-06-18 20:58:56 +0200

    filegrab.js

---
+++ js/bkm/filegrab.js
@@ -0,0 +1,23 @@
+filtra = function(s){
+	if (typeof s == "undefined") return 0;
+	if (typeof s.indexOf == "undefined") return 0;
+	if (s.indexOf(".mp4")>-1) return 1;
+	if (s.indexOf(".webm")>-1) return 1;
+	if (s.indexOf(".mkv")>-1) return 1;
+	if (s.indexOf(".flv")>-1) return 1;
+	return 0;
+	}
+
+
+all=document.querySelectorAll("a");
+linx=[];
+for (e in all) {
+if (filtra(all[e].href)) linx.push(all[e].href);	
+}
+
+	
+localStorage.setItem("VIDS"+document.location.href,linx);
+alert("ok");
+
+	
+
